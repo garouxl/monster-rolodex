b7fb2a7d1319e369622984aaf9761b88230e79f5
Leandro Almeida <garou_xl@yahoo.com.br>
2019-12-17 16:43:05 -0300

    Updates

---
+++ precache-manifest.09b4232a9fe8ead45a2117faa30e5cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdd3f4542b120824f5bdbc10144bb7d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bec7bffc5fbc1dc76167",
+    "url": "/monster-rolodex/static/css/main.82d6c0d5.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bec7bffc5fbc1dc76167",
+    "url": "/monster-rolodex/static/js/main.443e1299.chunk.js"
+  },
+  {
+    "revision": "939c7cac93c69c649cbc",
+    "url": "/monster-rolodex/static/js/runtime-main.11039a90.js"
+  }
+]);
